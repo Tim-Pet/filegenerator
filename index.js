23b6fb98751dd1cd235d8ed9b0a589b9c3c5a89f
Tim Petersen <tim.petersen1@outlook.de>
2021-05-26 14:39:12 +0200

    Add fileType to inquirer question

--- index.js
+++ index.js
@@ -4,14 +4,26 @@ const inquirer = require('inquirer')
 const questions = [
   {
     type: 'input',
-    name: 'file',
+    name: 'fileName',
     message:
       'Which function and file would you like to create? Seperate by comma!',
+  },
+  {
+    type: 'checkbox',
+    message: 'select file type',
+    name: 'fileTypes',
+    choices: [{ name: 'component' }, { name: 'spec' }, { name: 'stories' }],
+    validate(answer) {
+      if (answer.length < 1) {
+        return 'You must choose at least one fileType'
+      }
+      return true
+    },
   },
 ]
 
 inquirer.prompt(questions).then(answers => {
-  answers['file']
+  answers['fileName']
     .split(',')
     .forEach(answerEntry => writeFile(answerEntry.trim()))
 })
