9cecb75f1c57cf1316fd2bf49e01079ac880e2ff
Tim Petersen <tim.petersen1@outlook.de>
2021-05-26 10:07:31 +0200

    Update to current progress

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello world')
+const functionName = process.argv[2] ?? 'myFunction'
+
+// console.log(process.argv)
+/*
+[
+    '/opt/homebrew/lib/node_modules/node/bin/node',
+    '/Users/macfrei/neuefische/coaches/filegenerator/index.js',
+    'multiply'
+]
+*/
+
+console.log(`function ${functionName}() {
+}
+`)
